8cc06bac46e5bc9380f351360eb7f572b0773ac0
gregbakerstl <gregbakerstl@gmail.com>
2019-08-19 21:35:36 +0000

    bug fixes, portability enhancements

--- vim_ide_setup.sh
+++ vim_ide_setup.sh
@@ -2,11 +2,11 @@
 # gregbakerstl development environment setup
 
 # linux package list
-apt_packages='vim vim-gtk cmake python build-essential zsh'
+apt_packages='vim vim-gtk cmake ipython-dev python build-essential zsh'
 
 # install linux dev packages
 for package in $apt_packages; do
-	sudo apt install -y 
+	sudo apt install -y $package
 done
 
 # configure vim plugins and syntax highlighting
@@ -17,7 +17,7 @@ cat ${vim_config} >> ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # compile vundle
-python ~/.vim/bundle/YouCompleteMe/install.py
+python ~/.vim/bundle/YouCompleteMe/install.py --clang-completer 
 
 # compile and install plugins for vim
 vim +PluginInstall +qall
