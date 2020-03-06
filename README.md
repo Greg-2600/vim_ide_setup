36983c0c7a3348ee2c46152c5233873c0c52a0f5
gregbakerstl <gregbakerstl@gmail.com>
2020-03-06 11:29:30 -0600

    abstraction, idempotent, tested

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vim_ide_setup
 gregbakerstl's favorite small development environment installation process
 
-Usage:
+Idempotent Usage:
 
 bash vim_ide_setup.sh
--- vim_ide_setup.sh
+++ vim_ide_setup.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
 # gregbakerstl development environment setup
 
-# linux package list
-apt_packages='vim vim-gtk cmake python-dev python build-essential zsh shellcheck'
+install_packages() {
+	# linux package list
+	apt_packages='vim
+		      vim-gtk
+		      cmake
+		      python 
+		      python-dev
+		      build-essential
+		      zsh
+		      shellcheck'
 
-# install linux dev packages
-for package in $apt_packages; do
-	sudo apt install -y "$package"
-done
+	# install linux dev packages
+	for package in $apt_packages; do
+		sudo apt install -y "$package"
+	done
+}
 
-# configure vim plugins and syntax highlighting
-vim_config=vimrc
-cat ${vim_config} >> ~/.vimrc
 
-# pull down vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+configure_vim() {
+	# configure vim plugins and syntax highlighting
+	vim_config=vimrc
+	cat ${vim_config} >> ~/.vimrc
 
-# compile vundle
-python ~/.vim/bundle/YouCompleteMe/install.py --clang-completer 
+	# pull down vundle
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# compile and install plugins for vim
-vim +PluginInstall +qall
+	# compile YouCompleteMe
+	cd ~/.vim/bundle/YouCompleteMe || exit
+	./install.py
+	cd || exit
 
-# install oh my zsh
-echo "y"|sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	# compile and install plugins for vim
+	vim +PluginInstall +qall
+
+}
+
+
+install_zsh() {
+	# install oh my zsh
+	echo "y"|sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+
+main () {
+	install_packages
+	configure_vim
+	install_zsh
+}
+
+main
